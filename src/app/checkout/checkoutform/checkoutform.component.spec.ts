acaa49bb981df691b10bbe5ef9421a4fadab94fe
saadk6401 <saadk6401@gmail.com>
2024-01-02 12:15:50 +0500

    Initial commit

---
+++ src/app/checkout/checkoutform/checkoutform.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CheckoutformComponent } from './checkoutform.component';
+
+describe('CheckoutformComponent', () => {
+  let component: CheckoutformComponent;
+  let fixture: ComponentFixture<CheckoutformComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CheckoutformComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CheckoutformComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
