03ac13e6e43cd2cce6e9e688f56e35095fb1d6c5
unknown <kaleemikennels@gmail.com>
2024-01-04 15:54:54 +0500

    customer care

---
+++ src/app/customercare/customercare.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustomercareRoutingModule } from './customercare-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CustomercareRoutingModule
+  ]
+})
+export class CustomercareModule { }
