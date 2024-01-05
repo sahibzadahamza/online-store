5389b01b7230fe6e3ee665ef316d46b5f7466b38
unknown <kaleemikennels@gmail.com>
2024-01-05 19:40:59 +0500

    UI fixed

--- src/app/headermodule/headermodule-routing.module.ts
+++ src/app/headermodule/headermodule-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 const routes: Routes = [
   {path:'about-us' , component:AboutusComponent},
+  {path:'contactus' , component:ContactusComponent}
   ];
 
 @NgModule({
--- src/app/headermodule/headermodule.module.ts
+++ src/app/headermodule/headermodule.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HeadermoduleRoutingModule } from './headermodule-routing.module';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 
 @NgModule({
   declarations: [
     AboutusComponent,
+    ContactusComponent,
   ],
   imports: [
     CommonModule,
