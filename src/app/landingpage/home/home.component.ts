af210953f18b1478c7738828ab715f35e4bcc18d
unknown <kaleemikennels@gmail.com>
2024-01-25 11:13:38 +0500

    discount donw

--- src/app/landingpage/home/home.component.ts
+++ src/app/landingpage/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +10,11 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit{
-  firstTwoProducts: any;
+  firstTwoProducts: any[]=[];
+  discount: any;
+  price: any;
   
-  constructor(private router:Router, private productservice: ProductService){}
+  constructor(private router:Router, private productservice: ProductService, private cartservice: CartService){}
   ngOnInit(): void {
     this.productservice.getProducts().subscribe(res=>{
       console.log("these are products", res)
@@ -20,16 +24,17 @@ export class HomeComponent implements OnInit{
       const products: any[] | any = res;
   
       // Get only the first two products
-      this.firstTwoProducts = products.slice(222, 224);
+      this.firstTwoProducts = products.slice(0, 4);
   
       console.log("These are the first two products in home", this.firstTwoProducts);
   });
   
   }
 
-  gotodetails(){
-    this.router.navigate(['categories/details'])
+  gotodetails(_id:string){
+    this.router.navigate(['categories/details/' + _id])
   }
+  
 
 
 }
