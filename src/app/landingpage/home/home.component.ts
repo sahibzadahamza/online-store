1d0197f8fc93ea942453965cf7f27e1afec8d158
unknown <kaleemikennels@gmail.com>
2024-03-10 13:35:39 +0500

    new changes done

--- src/app/landingpage/home/home.component.ts
+++ src/app/landingpage/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CartService } from 'src/app/services/cart.service';
+import { CategoryService } from 'src/app/services/category.service';
 import { ProductService } from 'src/app/services/product.service';
 import Swal from 'sweetalert2';
 
@@ -13,9 +14,18 @@ export class HomeComponent implements OnInit{
   firstTwoProducts: any[]=[];
   discount: any;
   price: any;
-  
-  constructor(private router:Router, private productservice: ProductService, private cartservice: CartService){}
+  categories: any[] =[];
+  products: any[] = [];
+  displayItemCount: number = 12;
+  displayItemCount2: number = 3;
+  currentIndex: number = 0;
+  currentIndex2: number = 0;
+  constructor(private router:Router, private productservice: ProductService, private cartservice: CartService, private categoryservice:CategoryService){}
   ngOnInit(): void {
+    this.categoryservice.getCategory().subscribe((res:any)=>{
+      this.categories = res
+      console.log("these are categorueis in product" , res)
+    })
     this.productservice.getProducts().subscribe(res=>{
       console.log("these are products", res)
     })
@@ -28,13 +38,58 @@ export class HomeComponent implements OnInit{
   
       console.log("These are the first two products in home", this.firstTwoProducts);
   });
-  
+  this.updateDisplayItemCount();
+  this.updateDisplayItemCount2()
+  window.addEventListener('resize', () => this.updateDisplayItemCount());
+  window.addEventListener('resize', () => this.updateDisplayItemCount2());
+ 
   }
-
+  navigateToProduct(id: any) {
+    this.router.navigate(['/categories/products', id]); // Assuming there is an 'id' property in your product object
+  }
   gotodetails(_id:string){
     this.router.navigate(['categories/details/' + _id])
   }
-  
+  updateDisplayItemCount() {
+    // Adjust displayItemCount based on screen size
+    this.displayItemCount = window.innerWidth < 600 ? 3 : 12;
+    this.currentIndex = 0;
+  }
 
+  updateDisplayItemCount2() {
+    // Adjust displayItemCount based on screen size
+    this.displayItemCount2 = window.innerWidth < 600 ? 1 : 3;
+    this.currentIndex = 0;
+  }
+
+  showPrevButton(): boolean {
+    return this.categories.length > this.displayItemCount && this.currentIndex > 0;
+  }
+
+  showNextButton(): boolean {
+    return (
+      this.categories.length > this.displayItemCount &&
+      this.currentIndex < this.categories.length - this.displayItemCount
+    );
+  }
+  prev() {
+    this.currentIndex = (this.currentIndex - 1 + this.categories.length) % this.categories.length;
+
+      // this.currentIndex -= (this.currentIndex + 1) % this.displayItemCount;
+
+  }
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.categories.length;
+      // this.currentIndex += this.displayItemCount;
+
+  }
+  showproductsofcategory(id : any){
+    console.log("id" , id)
+    this.productservice.getProductByCategory(id).subscribe((res:any)=>{
+       console.log("selected category" , res)
+       this.products = res
+    })
+  }
 
 }
