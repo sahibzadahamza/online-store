8606f1673f894b6ac896e4994850fc3df5f7afb5
adnan khan <adnanmaneri2015@gmail.com>
2024-06-24 17:17:39 +0500

    new-chagnes

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   name: "",
-  apiUrl: '',
+  apiUrl: 'https://valvador-backend.onrender.com/api',
   socketUrl : '',
   stripeClientId:'',
   stripeKey:''
